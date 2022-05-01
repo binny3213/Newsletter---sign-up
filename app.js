62a1fe1558b791452723335f1e9a809712d8ceaa
binny3213 <binny3213@gmail.com>
2022-05-01 21:19:34 +0300

    First commit

---
+++ app.js
@@ -0,0 +1,74 @@
+//js hint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+
+const app= express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+app.get("/", function(req,res){
+  res.sendFile(__dirname+ "/signup.html");
+})
+
+app.post("/", function(req,res){
+  const firstName= req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
+
+  const data = {
+    members: [
+      {
+        email_address: email ,
+        status: "subscribed",
+        merge_feilds: {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  };
+
+  const jasonData = JSON.stringify(data);
+
+  const url = "https://us17.api.mailchimp.com/3.0/lists/6e62791c49";
+
+  const options = {
+    method: "POST",
+    auth : "binny1:b241ff6a0deb476df840d11a01c7df07-us17"
+  }
+
+  const request= https.request(url, options, function(response){
+    if(response.statusCode === 200){
+      res.sendFile(__dirname + "/success.html");
+    }else{
+      res.sendFile(__dirname + "/failure.html");
+    }
+      response.on("data", function(data){
+        console.log(JSON.parse(data));
+      })
+  })
+
+  request.write(jasonData);
+  request.end();
+});
+
+app.post("/failure", function(req,res){
+  res.redirect("/");
+})
+
+app.listen(process.env.PORT || 3000 , function(){
+  console.log("Server is running on port 3000");
+})
+
+
+//API key
+//03dae49cf76af0917d29a4d47e972f57-us17
+
+//b241ff6a0deb476df840d11a01c7df07-us17
+
+// lisd ID
+//6e62791c49
